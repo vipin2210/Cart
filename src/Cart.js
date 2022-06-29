198e3af635eafa9cf9d4a25c15c7f3f0a2b74e46
Vipin <2002vipin318@gmail.com>
2022-06-29 15:06:33 +0530

    ADD Cart and Add props to CarItems

--- src/Cart.js
+++ src/Cart.js
@@ -2,12 +2,49 @@ import React from "react";
 import CartItem from "./CartItem";
 
 class Cart extends React.Component {
+
+    constructor() {
+        super();
+        this.state = {
+            products: [
+                {
+                    price: 999,
+                    title: 'Mobile phone',
+                    qty: 1,
+                    img: '',
+                    id: 1
+                }
+                ,
+                {
+                    price: 99,
+                    title: 'Watch',
+                    qty: 4,
+                    img: '',
+                    id: 2
+                }, {
+                    price: 990,
+                    title: 'Laptop',
+                    qty: 8,
+                    img: '',
+                    id: 3
+                }
+            ]
+        }
+    }
+
     render() {
+        const { products } = this.state;
         return (
             <div className="cart">
-                <CartItem/>
-                <CartItem/>
-                <CartItem/>
+                {
+                    products.map((product) => {
+                        return (
+                            <CartItem
+                                product={product}
+                                key={product.id}
+                            />
+                        )
+                    })}
             </div>
         );
     }
--- src/CartItem.js
+++ src/CartItem.js
@@ -1,76 +1,66 @@
 import React from "react";
 
 class CartItem extends React.Component {
-    constructor(){
-        super();
-        this.state={
-        price:999,
-        title:'phone',
-        qty:1,
-        img:''
-        // increaseQuantity:this.increaseQuantity.bind(this);
-    }
-   }
-   
-    increaseQuantity=()=>{
-//this.state.qty+=1;
-    //    console.log('this.state',this.state);
-  //setState form 1
-    //     this.setState({
-    //     qty:this.state.qty+1
-    //   });
 
-//setState form 2 - if previous state required use it.
-this.setState((prevState)=>{
-return {
-    qty:prevState.qty+1
-}
-});
+    increaseQuantity = () => {
+        //this.state.qty+=1;
+        //    console.log('this.state',this.state);
+        //setState form 1
+        //     this.setState({
+        //     qty:this.state.qty+1
+        //   });
+
+        //setState form 2 - if previous state required use it.
+        this.setState((prevState) => {
+            return {
+                qty: prevState.qty + 1
+            }
+        });
 
     }
 
-    decreaseQuantity=()=>{
-        const {qty}=this.state;
-        if(qty==0){
+    decreaseQuantity = () => {
+        const { qty } = this.state;
+        if (qty == 0) {
             return;
         }
 
-        this.setState((prevState)=>{
+        this.setState((prevState) => {
             return {
-               qty:prevState.qty-1    
+                qty: prevState.qty - 1
             }
-            });
+        });
     }
 
     render() {
-        const {price,title,qty}=this.state;
+        const { price, title, qty } = this.props.product;
         return (
             <div className="cart-item">
                 <div className="left-block">
                     <img style={style.image} />
-                </div> 
+                </div>
                 <div className="right-block">
-                    <div style={{fontSize:25}}>{this.state.title}</div>
-                    <div style={{color:'#777'}}>Rs {price}</div>
-                    <div style={{color:'#777'}}>Qty: {qty}</div>
+                    <div style={{ fontSize: 25 }}>{title}</div>
+                    <div style={{ color: '#777' }}>Rs {price}</div>
+                    <div style={{ color: '#777' }}>Qty: {qty}</div>
                     <div className="cart-item-actions">
                         {/* Buttions */}
-                        <img 
-                            alt="increase" 
-                            className="actions-icons" 
+                        <img
+                            alt="increase"
+                            className="actions-icons"
                             src="https://cdn-icons-png.flaticon.com/512/992/992651.png"
                             onClick={this.increaseQuantity}
                         />
-                        <img 
-                            alt="decrease" 
-                            className="actions-icons" 
-                            src="https://cdn-icons-png.flaticon.com/512/992/992683.png" 
+                        <img
+                            alt="decrease"
+                            className="actions-icons"
+                            src="https://cdn-icons-png.flaticon.com/512/992/992683.png"
                             onClick={this.decreaseQuantity}
                         />
-                        
-                        <img 
-                            alt="delete" 
-                            className="actions-icons"  
+
+                        <img
+                            alt="delete"
+                            className="actions-icons"
                             src="https://cdn-icons-png.flaticon.com/512/1214/1214428.png"
 
                         />
@@ -86,7 +76,7 @@ const style = {
         height: 110,
         width: 110,
         borderRadius: 4,
-        background:'#ccc'
+        background: '#ccc'
     }
 }
 
